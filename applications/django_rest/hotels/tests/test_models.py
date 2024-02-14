4c9f25c17ce8f8612a63b0d9d77fc2e903a6b8c8
Jack Plowman <jack.plowman1@nhs.net>
2024-02-14 20:17:52 +0000

    Fix testing workflow

--- applications/django_rest/hotels/tests/test_models.py
+++ applications/django_rest/hotels/tests/test_models.py
@@ -13,9 +13,15 @@ class TestHotels(TestCase):
     def test_attributes(self: Self) -> None:
         hotel1 = Hotels.objects.create(name="Hotel1", address="Address1", phone="Phone1")
         assert hotel1.id is not None
+        assert Hotels._meta.get_field("id").primary_key
+        assert Hotels._meta.get_field("id").serialize is False
+        assert Hotels._meta.get_field("id").verbose_name == "ID"
         assert hotel1.name == "Hotel1"
+        assert Hotels._meta.get_field("name").max_length == 100
         assert hotel1.address == "Address1"
+        assert Hotels._meta.get_field("address").max_length == 100
         assert hotel1.phone == "Phone1"
+        assert Hotels._meta.get_field("phone").max_length == 100
 
     @pytest.mark.django_db()
     def test__str__(self: Self) -> None:
--- applications/django_rest/hotels/tests/test_urls.py
+++ applications/django_rest/hotels/tests/test_urls.py
@@ -4,3 +4,4 @@ from hotels.urls import urlpatterns
 def test_urls() -> None:
     assert urlpatterns is not None
     assert len(urlpatterns) == 1
+    assert urlpatterns.__getitem__(0).pattern._route == "hotels"
