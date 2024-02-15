f251c4671412cfde7667333781f33314dbe8b62a
Jack Plowman <jack.plowman1@nhs.net>
2024-02-15 09:01:23 +0000

    Add post api

--- applications/django_rest/hotels/tests/test_views.py
+++ applications/django_rest/hotels/tests/test_views.py
@@ -15,3 +15,17 @@ class TestHotelsApiView(TestCase):
         response = view.get(None)
         assert response.status_code == 200
         assert response.data == []
+
+    @pytest.mark.django_db()
+    def test_post(self: Self) -> None:
+        view = HotelsApiView()
+        request = type("Request", (), {"data": {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}})
+        response = view.post(request)
+        assert response.status_code == 201
+        assert response.data["id"] is not None
+        assert response.data["id"] > 0
+        response.data.pop("id")
+        assert response.data == {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}
+        assert response.data["name"] == "Hotel1"
+        assert response.data["address"] == "Address1"
+        assert response.data["phone"] == "Phone1"
--- applications/django_rest/hotels/views.py
+++ applications/django_rest/hotels/views.py
@@ -1,6 +1,6 @@
 from typing import Self
-
-from django.http import HttpRequest
+from rest_framework import status
+from django.http import HttpRequest, HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,8 +11,24 @@ from .serializer import HotelsSerializer
 class HotelsApiView(APIView):
     """View for the hotel model."""
 
+    # permission_classes = [permissions.IsAuthenticated]
+
     def get(self: Self, _request: HttpRequest) -> Response:
         """Return a list of all hotels."""
         hotels = Hotels.objects.all()
         serializer = HotelsSerializer(hotels, many=True)
         return Response(serializer.data)
+
+    def post(self: Self, request: HttpRequest, *args, **kwargs) -> HttpResponse:  # noqa: ANN002, ARG002, ANN003
+        """Create a new hotel."""
+        data = {
+            "name": request.data.get("name"),
+            "address": request.data.get("address"),
+            "phone": request.data.get("phone"),
+        }
+        serializer = HotelsSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
