2b6cc99bfdfd733b50bc1e4ab9ec74e7b8628e4e
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-02-15 10:05:01 +0000

    Add Advanced Django Rest APIs (#10)
    Adds, Post, Get (single), Put, and Delete REST methods

--- applications/django_rest/hotels/tests/test_views.py
+++ applications/django_rest/hotels/tests/test_views.py
@@ -1,17 +1,89 @@
 from typing import Self
-from unittest import TestCase
 
 import pytest
 
-from hotels.views import HotelsApiView
+from hotels.views import HotelsDetailApiView, HotelsListApiView
 
 
-class TestHotelsApiView(TestCase):
-    """Test case for the HotelsApiView view."""
+class TestHotelsListApiView:
+    """Test case for the HotelsListApiView view."""
 
     @pytest.mark.django_db()
     def test_get(self: Self) -> None:
-        view = HotelsApiView()
+        view = HotelsListApiView()
         response = view.get(None)
         assert response.status_code == 200
         assert response.data == []
+
+    @pytest.mark.django_db()
+    def test_post(self: Self) -> None:
+        view = HotelsListApiView()
+        request = type("Request", (), {"data": {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}})
+        response = view.post(request)
+        assert response.status_code == 201
+        assert response.data["id"] is not None
+        assert response.data["id"] > 0
+        response.data.pop("id")
+        assert response.data == {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}
+        assert response.data["name"] == "Hotel1"
+        assert response.data["address"] == "Address1"
+        assert response.data["phone"] == "Phone1"
+
+
+class TestHotelsDetailApiView:
+    """Test case for the HotelsDetailApiView view."""
+
+    @pytest.mark.django_db()
+    def test_get_hotel_not_found(self: Self) -> None:
+        view = HotelsDetailApiView()
+        request = type("Request", (), {"data": {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}})
+        response = view.get(request, 1)
+        assert response.status_code == 400
+        assert response.data == {"response": "Object with hotel id does not exists"}
+
+    @pytest.mark.django_db()
+    def test_get(self: Self) -> None:
+        view = HotelsListApiView()
+        request = type("Request", (), {"data": {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}})
+        response = view.post(request)
+        hotel_id = response.data["id"]
+        view = HotelsDetailApiView()
+        request = type("Request", (), {"data": {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}})
+        response = view.get(request, hotel_id)
+        assert response.status_code == 200
+        assert response.data["id"] == hotel_id
+        response.data.pop("id")
+        assert response.data == {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}
+        assert response.data["name"] == "Hotel1"
+        assert response.data["address"] == "Address1"
+        assert response.data["phone"] == "Phone1"
+
+    @pytest.mark.django_db()
+    def test_put(self: Self) -> None:
+        view = HotelsListApiView()
+        request = type("Request", (), {"data": {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}})
+        view.post(request)
+        hotel_id = 1
+        view = HotelsDetailApiView()
+        request = type("Request", (), {"data": {"name": "Hotel2", "address": "Address2", "phone": "Phone2"}})
+        response = view.put(request, hotel_id)
+        assert response.status_code == 200
+        assert response.data["id"] == hotel_id
+        response.data.pop("id")
+        assert response.data == {"name": "Hotel2", "address": "Address2", "phone": "Phone2"}
+        assert response.data["name"] == "Hotel2"
+        assert response.data["address"] == "Address2"
+        assert response.data["phone"] == "Phone2"
+
+    @pytest.mark.django_db()
+    def test_delete(self: Self) -> None:
+        view = HotelsListApiView()
+        request = type("Request", (), {"data": {"name": "Hotel1", "address": "Address1", "phone": "Phone1"}})
+        response = view.post(request)
+        hotel_id = response.data["id"]
+        view = HotelsDetailApiView()
+        response = view.delete(None, hotel_id)
+        assert response.status_code == 200
+        response = view.get(None, hotel_id)
+        assert response.status_code == 400
+        assert response.data == {"response": "Object with hotel id does not exists"}
--- applications/django_rest/hotels/views.py
+++ applications/django_rest/hotels/views.py
@@ -1,18 +1,83 @@
 from typing import Self
 
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Hotels
 from .serializer import HotelsSerializer
 
+HOTEL_NOT_FOUND = "Object with hotel id does not exists"
 
-class HotelsApiView(APIView):
+
+class HotelsListApiView(APIView):
     """View for the hotel model."""
+
+    # permission_classes = [permissions.IsAuthenticated]  # noqa: ERA001
 
     def get(self: Self, _request: HttpRequest) -> Response:
         """Return a list of all hotels."""
         hotels = Hotels.objects.all()
         serializer = HotelsSerializer(hotels, many=True)
         return Response(serializer.data)
+
+    def post(self: Self, request: HttpRequest, *args, **kwargs) -> HttpResponse:  # noqa: ANN002, ARG002, ANN003
+        """Create a new hotel."""
+        data = {
+            "name": request.data.get("name"),
+            "address": request.data.get("address"),
+            "phone": request.data.get("phone"),
+        }
+        serializer = HotelsSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class HotelsDetailApiView(APIView):
+    """View for the hotel model."""
+
+    # permission_classes = [permissions.IsAuthenticated]  # noqa: ERA001
+
+    def get_object(self: Self, hotel_id: int) -> Hotels | None:
+        """Helper method to get the object with given hotel_id."""
+        try:
+            return Hotels.objects.get(id=hotel_id)
+        except Hotels.DoesNotExist:
+            return None
+
+    def get(self: Self, request: HttpRequest, hotel_id: int, *args, **kwargs) -> HttpResponse:  # noqa: ANN002, ARG002, ANN003
+        """Retrieves the Hotel with given hotel_id if exists."""
+        hotel_instance = self.get_object(hotel_id)
+        if not hotel_instance:
+            return Response({"response": HOTEL_NOT_FOUND}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = HotelsSerializer(hotel_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self: Self, request: HttpRequest, hotel_id: int, *args, **kwargs) -> HttpResponse:  # noqa: ANN002, ARG002, ANN003
+        """Updates the hotel item with given hotel_id if exists."""
+        hotel_instance = self.get_object(hotel_id)
+        if not hotel_instance:
+            return Response({"response": HOTEL_NOT_FOUND}, status=status.HTTP_400_BAD_REQUEST)
+        data = {
+            "name": request.data.get("name"),
+            "address": request.data.get("address"),
+            "phone": request.data.get("phone"),
+        }
+        serializer = HotelsSerializer(instance=hotel_instance, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self: Self, request: HttpRequest, hotel_id: int, *args, **kwargs) -> HttpResponse:  # noqa: ANN002, ARG002, ANN003
+        """Deletes the hotel item with given hotel_id if exists."""
+        hotel_instance = self.get_object(hotel_id)
+        if not hotel_instance:
+            return Response({"response": HOTEL_NOT_FOUND}, status=status.HTTP_400_BAD_REQUEST)
+        hotel_instance.delete()
+        return Response({"response": "Object deleted!"}, status=status.HTTP_200_OK)
