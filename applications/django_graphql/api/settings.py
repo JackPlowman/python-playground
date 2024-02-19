69d4b9575ba22e1597df5c2763e87d2ee974ea70
Jack Plowman <jack.plowman1@nhs.net>
2024-02-19 11:15:25 +0000

    Build images with gunicorn

--- applications/django_graphql/api/settings.py
+++ applications/django_graphql/api/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = getenv("SECRET_KEY", "development")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+]
 
 # Application definition
 INSTALLED_APPS = [
