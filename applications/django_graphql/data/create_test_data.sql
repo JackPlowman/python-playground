d606b2f3098c2452e910cb9ac75e2c3ecd5b1e52
Jack Plowman <jack.plowman1@nhs.net>
2024-02-26 13:29:27 +0000

    Fix some sql violations

--- applications/django_graphql/data/create_test_data.sql
+++ applications/django_graphql/data/create_test_data.sql
@@ -2,7 +2,7 @@ INSERT INTO blog_author (id, name) VALUES (1, 'John Doe'), (2, 'Jane Doe'), (3, 
 
 INSERT INTO blog_post (title, content, author_id) VALUES
 ('First post', 'This is the first post', 1),
-('Second post', 'This is the second post',1),
+('Second post', 'This is the second post', 1),
 ('Third post', 'This is the third post', 1),
 ('Fourth post', 'This is the fourth post', 2),
 ('Fifth post', 'This is the fifth post', 2),
